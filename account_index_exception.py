dccd6954579b64ed1dc7342c2825a6bea01011ea
Marcel Suter <marcel.suter@bzz.ch>
2024-10-28 08:24:26 +0100

    fixed some tests

--- account_index_exception.py
+++ account_index_exception.py
@@ -1,4 +1,2 @@
-class AccountIndexException(Exception):
-
-    def __init__(self, index, max):
-        super().__init__(f'Ungültiger Indexwert: {max} Einträge vorhanden, Nummer {index} gefordert')
+class AccountIndexException():
+    pass
